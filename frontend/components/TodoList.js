6b646d0d36417605798a96c0311d446a6867dea5
Ladrillo <gabriel@cabrejas.net>
2022-06-15 18:48:16 +0200

    committing to "lecture"

--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -16,6 +16,7 @@ class TodoList extends React.Component {
         {
           todos.reduce((acc, td) => {
             const shouldDisplay = displayCompleteds || !td.completed
+            debugger
             if (shouldDisplay) return acc.concat(
               <Todo todo={td} key={td.id} />
             )
